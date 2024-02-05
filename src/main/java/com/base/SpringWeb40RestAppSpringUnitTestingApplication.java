4cfcaa85a14ed099c9419999860184a27113cb36
DeathRayy <avishal2713@gmail.com>
2024-02-05 18:11:35 +0530

    Initial Project created

---
+++ src/main/java/com/base/SpringWeb40RestAppSpringUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb40RestAppSpringUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeb40RestAppSpringUnitTestingApplication.class, args);
+	}
+
+}
